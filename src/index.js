96698f67702ce032b96eaf856b4fcd6d4a2e8866
scvkurt03 <kurtgray@gmail.com>
2022-01-14 13:05:10 -0800

    c/f toggle

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const location = document.querySelector('.location')
 const date = document.querySelector('.date')
 const time = document.querySelector('.time')
 const temp = document.querySelector('.temp')
-const units = document.querySelector('.units')
+const unitBtns = document.querySelector('.unit-buttons')
 const currentIcon = document.querySelector('.current-icon')
 const feelsTemp = document.querySelector('.feels-temp')
 const humidityPercent = document.querySelector('.humidity-percent')
@@ -15,24 +15,20 @@ const weekContainer = document.querySelector('.week-container')
 
 const key = 'b869ab386745e03cf8ad4414c45af90f'
 const defaultCity = 'Los Angeles'
+const defaultUnits = 'imperial'
+let currentCity = defaultCity
 
-// best place to put the following? utility.js module?
-let u = 'imperial' // for a toggle between units, used in the API request urls
-
-// view.js
 const updateUI = async (data) => {
-    const { cData, wData } = data
+    const { cData, wData, units } = data
     console.log(cData)
     console.log(wData)
-    console.log(wData.current.weather[0].main)
     
     main.innerHTML = `<h1>${wData.current.weather[0].main}</h1>`
     
     currentIcon.src = `http://openweathermap.org/img/wn/${wData.current.weather[0].icon}.png` 
     
-    // better to declare or do inline?
     const roundedTemp = Math.round(wData.current.temp)
-    temp.innerHTML = `<h1>${roundedTemp}º ${u === 'imperial' ? 'F' : 'C'}</h1>` // use ternary and change c/f inline
+    temp.innerHTML = `<h1>${roundedTemp}º ${units === 'imperial' ? 'F' : 'C'}</h1>` // use ternary and change c/f inline
 
     location.innerHTML = `<h3>${cData.name}, ${cData.sys.country}</h3>`
     
@@ -41,30 +37,36 @@ const updateUI = async (data) => {
     date.innerHTML = `<p>${dat.toDateString()}</p>`
     time.innerHTML = `<p>${dat.toLocaleTimeString()}</p>`
 
-    // need to develop C or F toggle
-    units.innerHTML = ``
+    unitBtns.innerHTML = `
+        <button class="units-toggle-c" id="metric">C</button>
+        <button class="units-toggle-f" id="imperial">F</button>
+    `
         
     const flk = Math.round(wData.current.feels_like)
-    feelsTemp.innerHTML = `${flk}º ${u === 'imperial' ? 'F' : 'C'}`
+    feelsTemp.innerHTML = `${flk}º ${units === 'imperial' ? 'F' : 'C'}`
 
     humidityPercent.innerHTML = `${wData.current.humidity}%`
     precipPercent.innerHTML = `${wData.daily[0].pop * 100}%`
     
     const roundWind = Math.round(wData.current.wind_speed)
-    windSpeed.innerHTML = `${roundWind} ${u === 'imperial' ? 'mph' : 'k/ph'}`
+    windSpeed.innerHTML = `${roundWind} ${units === 'imperial' ? 'mph' : 'k/ph'}`
+
+    weekContainer.innerHTML = ''
 
     const daily = wData.daily
     console.log(daily)
     
-    weekContainer.innerHTML = ''
-    
     for (let i = 1; i < daily.length; i++) {
+        const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        const forecastTimestamp = new Date(daily[i].dt * 1000)
+        const dayOfWeek = days[forecastTimestamp.getDay()]
+
         const dayContainer = document.createElement('div')
         dayContainer.classList.add('day-container')
         
         // figure out how to grab day from date
         const dayH3 = document.createElement('h3')
-        dayH3.innerText = 'Someday'
+        dayH3.innerText = `${dayOfWeek}`
         dayContainer.appendChild(dayH3)
 
         const highH1 = document.createElement('h1')
@@ -89,45 +91,54 @@ const updateUI = async (data) => {
 // where does this go? view 
 const getCityName = () => {
     const city = document.querySelector('.search-box').value;
+    currentCity = city
     return city
 }
 
 // API.js
-const getCoordinates = async (city) => {
+const getCoordinates = async (city, units) => {
     const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
-    const query = `?q=${city}&appid=${key}&units=${u}`
+    const query = `?q=${city}&appid=${key}&units=${units}`
     const response = await fetch(baseUrl + query)
     const cData = await response.json()
     return cData
 }
 
-const getWeather = async (lat, lon) => {
+const getWeather = async (lat, lon, units) => {
     const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall'
-    const query = `?lat=${lat}&lon=${lon}&appid=${key}&units=${u}` 
+    const query = `?lat=${lat}&lon=${lon}&appid=${key}&units=${units}` 
     const response = await fetch(baseUrl + query)
     const wData = await response.json()
     return wData
 }
 
-const fetchCityData = async (city) => {
-    const cData = await getCoordinates(city)
-    const wData = await getWeather(cData.coord.lat, cData.coord.lon)
-    return { cData, wData }
+const fetchCityData = async (city, units) => {
+    const cData = await getCoordinates(city, units)
+    const wData = await getWeather(cData.coord.lat, cData.coord.lon, units)
+    return { cData, wData, units }
 }
 
 export { fetchCityData }
 
 // index.js
 const init = async () => {
-    const data = await fetchCityData(defaultCity)
+    console.log(defaultUnits)
+    const data = await fetchCityData(defaultCity, defaultUnits)
     updateUI(data)
     
     const form = document.querySelector('.search-form')
     form.addEventListener('submit', async (e) => {
         e.preventDefault()
         const city = getCityName()
-        const data = await fetchCityData(city)
+        const data = await fetchCityData(city, defaultUnits)
         updateUI(data)
+        form.reset()
+    })
+    
+    unitBtns.addEventListener('click', async (e) => {
+        const units = e.target.id
+        const data = await fetchCityData(currentCity, units)
+        updateUI(data, units)
     })
 }
 init()
